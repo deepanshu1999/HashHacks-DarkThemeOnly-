70b5d9714ea5c59b30502eb99c0ada192ffc18dd
vinamra bathwal <vinamrabathwal99@gmail.com>
2018-10-26 22:29:59 +0530

    login made

--- app/src/main/java/com/rohan/hashhacks30/Login.java
+++ app/src/main/java/com/rohan/hashhacks30/Login.java
@@ -1,14 +1,53 @@
 package com.rohan.hashhacks30;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class Login extends AppCompatActivity {
+public int RC_SIGN_IN=123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
--- app/src/main/java/com/rohan/hashhacks30/MainActivity.java
+++ app/src/main/java/com/rohan/hashhacks30/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_activity);
-        Intent i=new Intent(MainActivity.this,PersonInfo.class);
-        startActivity(i);
+        
     }
 }
