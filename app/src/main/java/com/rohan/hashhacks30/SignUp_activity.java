32c2c9a70b4701f6f513080acb7e4eb30c426272
Rohan <rohan_dce@outlook.com>
2018-10-26 19:08:05 +0530

    SignUp

--- app/src/main/java/com/rohan/hashhacks30/SignUp_activity.java
+++ app/src/main/java/com/rohan/hashhacks30/SignUp_activity.java
@@ -3,7 +3,7 @@ package com.rohan.hashhacks30;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUp_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
